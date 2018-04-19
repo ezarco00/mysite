a2f0a357cf93766c26d68aff7bc89fd6a328a5a8
Emely Zarco <ezarco00@24dcaea7d349>
2018-04-19 17:03:45 +0000

    Converted class schedule page to a template.

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+import constants
 
 app = Flask(__name__)
 
@@ -10,6 +12,11 @@ def hello_world():
 def about_me():
     return app.send_static_file('about_me.html')
 
+#@app.route('/class_schedule')
+#def class_schedule():
+    #return app.send_static_file('class_schedule.html')
+
 @app.route('/class_schedule')
 def class_schedule():
-    return app.send_static_file('class_schedule.html')
+    return render_template('class_schedule.html',
+                           courses=constants.COURSES)
