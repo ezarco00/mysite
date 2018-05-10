ef892abe2d52a1819b330f7d587483084806f6f2
Emely Zarco <ezarco00@24dcaea7d349>
2018-05-10 15:44:03 +0000

    move top ten songs into database

--- flask_app.py
+++ flask_app.py
@@ -15,6 +15,12 @@ class Course(db.Model):
     resource_name = db.Column(db.String(80))
     resource_url = db.Column(db.String(300))
 
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Integer(80))
+    artist = db.Column(db.String(80))
+    youtube_url = db.Column(db.String(300))
+
 @app.route('/')
 def hello_world():
     return 'Hello from Emely'
@@ -29,11 +35,13 @@ def register():
 
 @app.route('/class_schedule')
 def class_schedule():
+    courses = Course.query.all()
     return render_template('class_schedule.html',
-                           courses=constants.COURSES)
+                           courses=courses)
 @app.route('/top_ten_songs')
 def top_ten_songs():
+    songs = Song.query.all()
     return render_template('top_ten_songs.html',
-                            songs=constants.Top_Ten_Songs)
+                           songs=songs)
 if __name__ == '__main__':
   db.create_all()
