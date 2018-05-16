2f43eac082fbfed9f0f9436c562a26fb3ec5501e
Emely Zarco <ezarco00@24dcaea7d349>
2018-05-16 16:08:36 +0000

    Moved song information into database

--- database_import_songs.py
+++ database_import_songs.py
@@ -6,7 +6,7 @@ for song in constants.TOP_TEN_SONGS:
     db.session.add(
         Song(
             title=song.title,
-            artist_name=song.artist,
+            artist_name=song.artist_name,
             youtube_url=song.youtube_url))
 
 db.session.commit()
--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+
 from flask import render_template
+
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
 
