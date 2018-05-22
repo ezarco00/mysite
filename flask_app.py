02aec9a611981b0a8b1c144bdfc38bccca604ef7
Emely Zarco <ezarco00@24dcaea7d349>
2018-05-22 16:09:37 +0000

    Created a login form and login page.

--- flask_app.py
+++ flask_app.py
@@ -19,6 +19,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, Length
 
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
+from flask import redirect
+from flask import url_for
+from wtforms.validators import ValidationError
+
+from flask import flash
+
 class RegistrationForm(FlaskForm):
     username = StringField(
         'Username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -27,11 +36,21 @@ class RegistrationForm(FlaskForm):
     password = PasswordField(
         'Password', validators=[InputRequired(), Length(min=8, max=80)])
     submit = SubmitField('Register')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please choose a different username.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Sign in')
 
 nav = Nav(app)
 @nav.navigation('mysite_navbar')
 def create_navbar():
     home_view = View('Home', 'homepage')
+    login_view = View('Login', 'login')
     register_view = View('Register', 'register')
     about_me_view = View('About Me', 'about_me')
     class_schedule_view = View('Class Schedule', 'class_schedule')
@@ -40,7 +59,7 @@ def create_navbar():
                              about_me_view,
                              class_schedule_view,
                              top_ten_songs_view)
-    return Navbar('MySite', home_view, misc_subgroup, register_view)
+    return Navbar('MySite', home_view, misc_subgroup, login_view, register_view)
 
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
@@ -59,9 +78,32 @@ class Song(db.Model):
     artist_name = db.Column(db.String(80))
     youtube_url = db.Column(db.String(300))
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15))
+    email = db.Column(db.String(150))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 @app.route('/')
 def homepage():
     return 'Hello from Emely'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            flash('Username or password is incorrect.', 'danger')
+            return render_template('login.html', form=form)
+        return 'Welcome ' + user.username + '!'
+    return render_template('login.html', form=form)
 
 @app.route('/about_me')
 def about_me():
@@ -71,10 +113,13 @@ def about_me():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        return (
-            form.username.data + ', ' +
-            form.email.data + ', ' +
-            form.password.data)
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('homepage'))
     return render_template('register.html', form=form)
 
 @app.route('/class_schedule')
